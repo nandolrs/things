f4236be3a836eaba88abded3830285b903310a53
Fernando <fernandol.silva@procempa.com.br>
2023-10-24 15:43:41 -0300

    .

--- temperatura-pressao-altitude/estacao_aws_get_database.py
+++ temperatura-pressao-altitude/estacao_aws_get_database.py
@@ -1,13 +1,12 @@
 import json
 from pymongo import MongoClient
 from dateutil import parser
+import os
 
 def get_database():
  
    # Provide the mongodb atlas url to connect python to mongodb using pymongo
-   CONNECTION_STRING = "localhost:27017"# "mongodb+srv://user:pass@cluster.mongodb.net/myFirstDatabase"
-   #CONNECTION_STRING = "mongodb://nandolrs:12c7664!@docdb-cmj.cluster-cfiihtdzyscs.sa-east-1.docdb.amazonaws.com:27017/?tls=true&tlsCAFile=/opt/python/global-bundle.pem&replicaSet=rs0&readPreference=secondaryPreferred&retryWrites=false"
-   #CONNECTION_STRING = "mongodb://nandolrs:12c7664!@docdb-cmj.cluster-cfiihtdzyscs.sa-east-1.docdb.amazonaws.com:27017/?replicaSet=rs0&readPreference=secondaryPreferred&retryWrites=false"
+   CONNECTION_STRING = os.environ['MONGO_CONN']
 
    # Create a connection using MongoClient. You can import MongoClient or use pymongo.MongoClient
    client = MongoClient(CONNECTION_STRING)
