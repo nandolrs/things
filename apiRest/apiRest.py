14ac1819bbdffddc51864f978025b69f4b416102
Fernando Rodrigues <34346597+nandolrs@users.noreply.github.com>
2024-11-19 12:18:47 -0300

    .

--- apiRest/apiRest.py
+++ apiRest/apiRest.py
@@ -27,14 +27,22 @@ def read_root():
     return {"mensagem": "to aqui, pronto e saudável"}
 
 @app.post("/api/clima")
-async def create_item(entidade: Clima) :
+async def incluir_item(entidade: Clima) :
     Incluir('Clima',entidade, 1)
     return entidade
 
 @app.put("/api/clima")
-async def create_item(entidade: Clima) :
+async def alterar_item(entidade: Clima) :
     Alterar('Clima',entidade, 1)
     return entidade
+
+@app.get("/api/clima/{id}")
+async def consultar_item(id: int) :
+    entidade = Consultar('Clima',id, 0)
+    return entidade
+
+
+
 
 def parse_float(value) :
     return Decimal(str(value))
@@ -64,15 +72,13 @@ def IncluirID(tabelaNome) :
 
 def ConsultarID(tabelaNome) :
 
-    #table = DynamoTabela('TabelasIndices')
-
     scan = DynamoBuscaS('TabelasIndices', Attr('tabelaNome').eq(tabelaNome) ) #table.scan(FilterExpression=Attr('tabelaNome').eq(tabelaNome))
 
     retorno = 0
 
     if len(scan['Items']) == 0 : # adicionar
         retorno = IncluirID(tabelaNome)
-    else :
+    else : # alterar
         documento = scan['Items'][0]
         documento['ultimoIndice'] = documento['ultimoIndice'] + 1
         Alterar('TabelasIndices', documento,0)
@@ -110,6 +116,22 @@ def Alterar(nomeTabela, entidade, api) :
 
     table.put_item(Item=entidadeDicionario)
     
+def Consultar(nomeTabela, ID, api) :
+
+    table = DynamoTabela(nomeTabela)
+
+    entidade = {'id': ID}
+
+    if (api == 1) :
+        entidadeDicionario = entidade.dict()
+    else :
+        entidadeDicionario = entidade
+
+    #entidadeDicionario : dict = json.loads(json.dumps(entidade), parse_float=parse_float)
+
+    retorno = table.get_item(Key=entidadeDicionario)    
+
+    return retorno['Item']
 
 if __name__ == "__main__":
     uvicorn.run(app, host="0.0.0.0", port=8000)
