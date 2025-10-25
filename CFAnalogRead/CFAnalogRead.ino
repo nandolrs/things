7824dd13bf9c7db7d72f4c73c44f23f54a588a4b
Fernando Rodrigues <34346597+nandolrs@users.noreply.github.com>
2025-10-25 07:58:26 -0300

    .

--- CFAnalogRead/CFAnalogRead.ino
+++ CFAnalogRead/CFAnalogRead.ino
@@ -1,12 +1,15 @@
 const int leituraPino = A0;// nodemcu esp-32 v3 = A0
-const int ledPino = 5;// nodemcu esp-32 v3 = GPIO4 = D1
+const int ledPino = 5;// nodemcu esp-32 v3 = GPIO5 = D1
+const int ledCondicaoPino = 2;// nodemcu esp-32 v3 = GPIO4 = D4
 
 long leitura;
+const int limiteCondicao = 1; //  
  
 void setup() {
   Serial.begin(9600); 
   pinMode(leituraPino, INPUT); 
   pinMode(ledPino, OUTPUT); 
+  pinMode(ledCondicaoPino, OUTPUT); 
 
 }
  
@@ -18,8 +21,13 @@ void loop() {
   Serial.println(leitura);
 
   digitalWrite(ledPino, HIGH);
+  if (leitura > limiteCondicao)
+  {
+    digitalWrite(ledCondicaoPino, HIGH);
+  }  
   delay(1000);
   digitalWrite(ledPino, LOW);
+  digitalWrite(ledCondicaoPino, LOW);
   delay(1000);
 
 }
